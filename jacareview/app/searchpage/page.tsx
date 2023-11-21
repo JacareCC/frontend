b30f4898b336042080988c74a258565ed90bd3a0
Will Brammer <williamedwardbrammer@gmail.com>
2023-11-21 21:56:37 +0900

    feat: implement get location button on submit

--- jacareview/app/searchpage/page.tsx
+++ jacareview/app/searchpage/page.tsx
@@ -5,11 +5,30 @@ import { useAuthState } from "react-firebase-hooks/auth"
 import { initFirebase } from "@/firebase/firebaseapp"
 
 export default function SearchPage() {
+    const [location, setLocation] = useState<any>(null);
+
+    useEffect(()=>{
+        console.log(location);
+    },[location])
+
     initFirebase();
     const auth = getAuth(); 
     const [user, loading] = useAuthState(auth)
 
+    function handleSubmitWithLocation(){
+        if('geolocation' in navigator) {
+            // Retrieve latitude & longitude coordinates from `navigator.geolocation` Web API
+            navigator.geolocation.getCurrentPosition(({ coords }) => {
+                const { latitude, longitude } = coords;
+                setLocation({ latitude, longitude });
+            })
+    }
+}
+
     return(
+        <>
         <div>Welcome {user?.displayName}</div>
+        <button onClick={handleSubmitWithLocation}>Location</button>
+        </>
     )
 }
