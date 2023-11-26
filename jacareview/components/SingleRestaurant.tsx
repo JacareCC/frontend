2c2cf9c8afe7315456b1cc65d7d652a1548b6e01
Will Brammer <williamedwardbrammer@gmail.com>
2023-11-26 15:24:24 +0900

    feat: implement graph that can be adjusted later to make doughnut

--- jacareview/components/SingleRestaurant.tsx
+++ jacareview/components/SingleRestaurant.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { Doughnut } from "react-chartjs-2";
+import { ArcElement } from "chart.js";
+import { Doughnut, Chart } from "react-chartjs-2";
 
 export default function SingleRestaurant({setSingleClicked, idForFetch}:{setSingleClicked: any, idForFetch:string}){
     const [singleRestaurantData, setSingleRestaurantData] = useState<any>(null);
@@ -7,7 +8,8 @@ export default function SingleRestaurant({setSingleClicked, idForFetch}:{setSing
     const [dataForChart, setDataForChart] = useState<any> (null)
 
     useEffect(() => {
-        fetchRestaurantData();
+        // fetchRestaurantData();
+        // postHistory();
         setDateVisited(new Date());
     }, []);
 
@@ -25,10 +27,25 @@ export default function SingleRestaurant({setSingleClicked, idForFetch}:{setSing
         id: idForFetch,
         date: dateVisited
     }
+    
+    const data = {
+        labels: ['Category A', 'Category B', 'Category C', 'Category D', 'Category E'],
+        datasets: [
+          {
+            data: [300, 50, 100, 40, 200],
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50', '#FF5733'],
+            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50', '#FF5733'],
+          },
+        ],
+      };
 
     const options = {
-
-    }
+        maintainAspectRatio: false,
+        responsive: true,
+        legend: {
+          position: 'right',
+        },
+      };
 
     const textCenter = {
         id: "textCenter",
@@ -74,13 +91,20 @@ async function postHistory() {
         <div>
         <div>PlaceHolder</div>
        <Doughnut
-       data={dataForChart}
+       data={data}
        options={options}
        plugins={[textCenter]}
        />
         </div>)
         
         }
+        <div>
+         <Doughnut
+       data={data}
+       options={options}
+       plugins={[textCenter]}
+       />
+        </div>
         <button onClick={goBack}>Back</button>
         </>
     )
