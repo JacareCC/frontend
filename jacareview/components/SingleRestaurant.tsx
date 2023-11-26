772b515702ebad7b9a1b7dcbb1552d14ee6fd29d
Will Brammer <williamedwardbrammer@gmail.com>
2023-11-26 13:37:49 +0900

    feat: implement post to save history

--- jacareview/components/SingleRestaurant.tsx
+++ jacareview/components/SingleRestaurant.tsx
@@ -2,14 +2,26 @@ import { useEffect, useState } from "react";
 
 export default function SingleRestaurant({setSingleClicked, idForFetch}:{setSingleClicked: any, idForFetch:string}){
     const [singleRestaurantData, setSingleRestaurantData] = useState<any>(null);
+    const [dateVisited, setDateVisited] = useState<Date | null> (null)
 
     useEffect(() => {
         fetchRestaurantData();
+        setDateVisited(new Date());
     }, []);
 
     //helper
     function goBack(){
         setSingleClicked((prev:boolean) => !prev);
+    }
+
+    interface postObject {
+        id: string,
+        date: Date | null,
+    }
+
+    const postObject: postObject = {
+        id: idForFetch,
+        date: dateVisited
     }
 
     async function fetchRestaurantData (){
@@ -21,6 +33,16 @@ export default function SingleRestaurant({setSingleClicked, idForFetch}:{setSing
           })
               .then(response => {return response.json()})
               .then(data => {setSingleRestaurantData(data) })
+}
+
+async function postHistory() {
+    const results = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}restauranthistory`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json" , 
+        },
+        body : JSON.stringify(postObject)
+      })
 }
 
     return (
