1d20f6a0b644d8a26d804c5ef5b85cf6970ab79d
Will Brammer <williamedwardbrammer@gmail.com>
2023-11-27 15:37:57 +0900

    feat: add endpoint for history seen

--- jacareview/components/RestaurantsSeen.tsx
+++ jacareview/components/RestaurantsSeen.tsx
@@ -1,7 +1,42 @@
 "use client"
+import {useState, useEffect} from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { initFirebase } from "@/firebase/firebaseapp";
+import { setuid } from "process";
 
 export default function RestaurantsSeen(){
+    const [historyData, SetHistoryData] = useState<any>(null);
+    const [uid, setUid] = useState<string|null |undefined> (null);
 
+    initFirebase();
+    const auth = getAuth(); 
+    const [user, loading] = useAuthState(auth);
+
+    useEffect(() => {
+        if(user){
+        setUid(user?.uid)
+        }
+    }, [user]);
+
+    useEffect(()=>{
+        getHistoryData();
+    }, [uid])
+
+    //helper 
+    async function getHistoryData(){
+        const results = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}user/history`, {
+            method: 'GET',
+            headers: {
+              "Content-Type": "application/json" , 
+              "Authorization" : `${uid}`
+            }
+          })
+              .then(response => {return response.json()})
+              .then(data => {SetHistoryData(data) });
+        }
+
+    
 
     return (
 
