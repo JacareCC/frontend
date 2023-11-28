f5b98adb9c4c00ced1ead8f7a73f4b2878d8e049
GONZALEZ-RODRIGUES <gonzalez.rodrigues@gmail.com>
2023-11-28 18:57:48 +0900

    feat: claim page

---
+++ jacareview/app/claimpage/page.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth"
+import { initFirebase } from "@/firebase/firebaseapp"
+import { useRouter, useSearchParams } from "next/navigation";
+import '../globals.css'
+import ReviewForm from "@/components/ReviewForm";
+import Navbar from "@/components/Navbar";
+// import { getDisplayName } from "next/dist/shared/lib/utils";
+import ClaimForm from "@/components/ClaimForm";
+
+
+export default function ClaimPage() {
+
+    initFirebase();
+    const auth = getAuth(); 
+    const [user, loading] = useAuthState(auth);
+    const [userUid, setUserUid] = useState<String | null> (null)
+    const router = useRouter();
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        setUserUid(uid);
+        } else {
+            router.push("/")
+        }
+    });
+
+    return (
+        <>
+            <Navbar /> 
+            <ClaimForm userUid={String(userUid)}  />
+        </>
+    )
+
+}
---
+++ jacareview/components/ClaimForm.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../app/globals.css';
+import TextInput from "./FormTextInput";
+import ClaimConfirm from "./ClaimConfirm";
+import { useRouter } from "next/navigation";
+
+interface ClaimFormProps {
+    userUid: string;
+}
+
+interface ClaimData {
+    user_uid: string;
+    first_name: string;
+    last_name: string;
+    business_name: string;
+    email: string;
+    contact_person: string;
+    address: string;
+    phone_number: string;
+}
+
+const ClaimForm: React.FC<ClaimFormProps> = ({ userUid }) => {
+    const { register, handleSubmit, setValue } = useForm<ClaimData>();
+    const router = useRouter();
+
+
+    const onSubmitHandler = async (data: ClaimData) => {
+        const results = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/business/claim`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => console.log(response));
+
+        toast.success('Claim submitted!'
+        , {
+            autoClose: 2000,
+        });
+        setTimeout(() => {
+            router.push('/userpage');
+        }, 3000); 
+
+        console.log(data);
+    }
+
+    useEffect(() => {
+        setValue('user_uid', userUid);
+    }, [userUid])
+
+    return (
+        <div className="w-[100vw] flex justify-center">
+            <form className="w-[100vw] flex flex-col" onSubmit={handleSubmit(onSubmitHandler)}>
+                <h2 className="font-semibold p-2 shadow-md bg-green-50">
+                    Claim your restaurant now and start leveraging customer reviews to boost your business.</h2>
+                <TextInput register={register} name='first_name' title='First Name' />
+                <TextInput register={register} name='last_name' title="Last Name" />
+                <TextInput register={register} name='email' title='Email' />
+                <TextInput register={register} name='business_name' title='Business Name' />
+                <TextInput register={register} name='address' title="Business Address" />
+                <TextInput register={register} name='contact_person' title='Contact Person' />
+                <TextInput register={register} name='phone_number' title='Phone number' />
+                <button className=" mx-8 mb-4 mt-4 bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600" type="submit">Submit</button>
+            </form>
+            <ToastContainer />
+        </div>
+    )
+}
+
+export default ClaimForm;
