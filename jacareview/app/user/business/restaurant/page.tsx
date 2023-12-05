1b8d1da734f40c5515d0c6cb8cc8e34670300edd
Will Brammer <williamedwardbrammer@gmail.com>
2023-12-05 23:41:40 +0900

    style: add some style to the page for business

--- jacareview/app/user/business/restaurant/page.tsx
+++ jacareview/app/user/business/restaurant/page.tsx
@@ -1,8 +1,12 @@
 "use client"
 import { useState, useEffect} from "react";
+import { useRouter, useSearchParams } from 'next/navigation';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth"
+import { initFirebase } from "@/firebase/firebaseapp"
 import Navbar from "@/components/Navbar";
 import BusinessNavBar from "@/components/BusinessNavBar";
-import { useRouter, useSearchParams } from 'next/navigation';
+import BusinessEditForm from "@/components/formComponents/BusinessEditForm";
 import OnlyOneOkButtonTier from "@/components/buttons/onlyOneOkButton/OnlyOneOkButtonTier";
 
 const BusinessPageWithId: React.FC = () => {
@@ -11,6 +15,10 @@ const BusinessPageWithId: React.FC = () => {
     const router = useRouter();
     const params = useSearchParams()
     const data = params.get("data");
+
+    initFirebase();
+    const auth = getAuth();
+    const [user, loading] = useAuthState(auth)
   
 
     useEffect(() =>{
@@ -34,54 +42,58 @@ const BusinessPageWithId: React.FC = () => {
     }
   
     return (
-      <div>
+      <div className="w-screen h-screen overflow-hidden">
         <Navbar />
   
        
-      <div className="mt-16 p-4 flex flex-col items-center">
-        <BusinessNavBar />
+        <div className="mt-16 p-4 flex flex-col items-center">
+  <BusinessNavBar />
 
-        {pageData && (
-          <div className="flex">
-            <div className="bg-black bg-opacity-50 absolute inset-0"></div>
-            <div className="bg-white p-8 rounded-md shadow-md relative">
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                <OnlyOneOkButtonTier
-                  id={pageData.owner_user_id_id}
-                  backgroundColor={'bronze'}
-                  text={'Bronze'}
-                  setStatusCode={setStatusCode}
-                />
-                <OnlyOneOkButtonTier
-                  id={pageData.owner_user_id_id}
-                  backgroundColor={'silver'}
-                  text={'Silver'}
-                  setStatusCode={setStatusCode}
-                />
-                <OnlyOneOkButtonTier
-                  backgroundColor={'gold'}
-                  id={pageData.owner_user_id_id}
-                  text={'Gold'}
-                  setStatusCode={setStatusCode}
-                />
-              </div>
-              <div className="flex justify-center items-center">
-              <button
-              key={"CloseButton"}
-              onClick={handleClose}
-              className={`${
-             'bg-jgreen' 
-              } text-white p-2 m-1 rounded`}
-                  >
-                  Close
-             </button>
-             </div>
-            </div>
-            
-          </div>
-          
-        )}
+  {pageData && (
+    <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center">
+      <div className="overflow-hidden bg-black bg-opacity-50 absolute inset-0"></div>
+      <div className="w-4/5 bg-white p-8 rounded-md shadow-md relative">
+        <div className="sm:flex flex-col items-center justify-center md:flex flex-row items-center justify-center">
+          <OnlyOneOkButtonTier
+            id={pageData.owner_user_id_id}
+            backgroundColor={'bronze'}
+            text={'Bronze'}
+            setStatusCode={setStatusCode}
+          />
+          <OnlyOneOkButtonTier
+            id={pageData.owner_user_id_id}
+            backgroundColor={'silver'}
+            text={'Silver'}
+            setStatusCode={setStatusCode}
+          />
+          <OnlyOneOkButtonTier
+            backgroundColor={'gold'}
+            id={pageData.owner_user_id_id}
+            text={'Gold'}
+            setStatusCode={setStatusCode}
+          />
+        </div>
+        <BusinessEditForm
+          email={pageData.email}
+          contactPerson={pageData.contact_person}
+          phoneNumber={pageData.phone_number}
+          user_uid={user?.uid}
+        />
+        <div className="flex justify-center items-center">
+          <button
+            key={"CloseButton"}
+            onClick={handleClose}
+            className={`${
+              'bg-jgreen'
+            } text-white p-2 m-1 rounded`}
+          >
+            Close
+          </button>
+        </div>
       </div>
+    </div>
+  )}
+</div>
 
 
       
---
+++ jacareview/components/formComponents/BusinessEditForm.tsx
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+import { UseFormRegister, FieldValues } from 'react-hook-form';
+
+interface BusinessEditFormProps {
+  user_uid?: string | any;
+  email?: string;
+  contactPerson?: string;
+  phoneNumber?: string | any;
+  placeholder?: string | undefined;
+  register?: UseFormRegister<FieldValues>;
+  onEditSave?: (data: { email?: string; contactPerson?: string; phoneNumber?: string }) => void;
+}
+
+const BusinessEditForm: React.FC<BusinessEditFormProps> = ({ email, contactPerson, phoneNumber, user_uid, register, onEditSave }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedEmail, setEditedEmail] = useState(email || '');
+  const [editedContactPerson, setEditedContactPerson] = useState(contactPerson || '');
+  const [editedPhoneNumber, setEditedPhoneNumber] = useState<string | undefined | any>(phoneNumber);
+
+  const [formData, setFormData] = useState({
+    user_uid: user_uid || '',
+    email: email || '',
+    contactPerson: contactPerson || '',
+    phoneNumber: phoneNumber || '',
+  });
+
+  const handleEditSaveClick = async () => {
+    if (isEditing) {
+      const data = {
+        email: editedEmail,
+        contactPerson: editedContactPerson,
+        phoneNumber: editedPhoneNumber,
+      };
+
+      setFormData((prevData) => ({
+        ...prevData,
+        ...data,
+      }));
+
+      if (onEditSave) {
+        onEditSave(data);
+      }
+
+      try {
+        const results = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}user/edit/`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: user_uid || '',
+          },
+          body: JSON.stringify(data),
+        });
+
+        if (results.ok) {
+          console.log('Changes saved successfully');
+        } else {
+          console.error('Error saving changes:', results.statusText);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+
+    setIsEditing(!isEditing);
+  };
+
+  useEffect(() => {
+    setEditedEmail(email || '');
+    setEditedContactPerson(contactPerson || '');
+    setEditedPhoneNumber(phoneNumber);
+  }, [email, contactPerson, phoneNumber]);
+
+  return (
+    <div className="flex flex-col gap-4 px-8 py-2 font-yaro">
+      <div className="flex flex-col align-center border-solid border-b border-gray-300 mb-2">
+        <label className='text-sm'>Email</label>
+        {isEditing ? (
+          <div className='mb-2 flex justify-between items-center w-full'>
+            <input
+              type='text'
+              value={editedEmail}
+              onChange={(e) => setEditedEmail(e.target.value)}
+              {...register && register('email')} 
+            />
+          </div>
+        ) : (
+          <div className='mb-2 flex justify-between items-center w-full'>
+            <p className='text-lg'>{editedEmail || "N/A"}</p>
+          </div>
+        )}
+      </div>
+      <div className="mb-2 flex flex-col align-center border-solid border-b border-gray-300">
+        <label className='text-sm'>Contact Person</label>
+        {isEditing ? (
+          <div className='flex justify-between items-center w-full'>
+            <input
+              type='text'
+              value={editedContactPerson}
+              onChange={(e) => setEditedContactPerson(e.target.value)}
+              {...register && register('contactPerson')} 
+            />
+          </div>
+        ) : (
+          <div className='flex justify-between items-center w-full'>
+            <p className='text-lg'>{editedContactPerson || "N/A"}</p>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col align-center border-solid border-b border-gray-300">
+        <label className='text-sm'>Phone Number</label>
+        {isEditing ? (
+          <div className='flex justify-between items-center w-full'>
+            <input
+              type='tel'
+              value={editedPhoneNumber}
+              onChange={(e) => setEditedPhoneNumber(e.target.value)}
+              {...register && register('phoneNumber')} 
+            />
+          </div>
+        ) : (
+          <div className='flex justify-between items-center w-full'>
+            <p className='text-lg'>{editedPhoneNumber || "N/A"}</p>
+          </div>
+        )}
+      </div>
+
+      <button className='bg-green-500 text-white p-2 rounded shadow-lg shadow-xl flex justify-center items-center' onClick={handleEditSaveClick}>
+        {isEditing ? 'Save' : 'Edit'}
+      </button>
+    </div>
+  );
+};
+
+export default BusinessEditForm;
