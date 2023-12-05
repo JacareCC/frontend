70a83f66e35023a1f78d22e80621b4b0142d70b2
GONZALEZ-RODRIGUES <gonzalez.rodrigues@gmail.com>
2023-12-05 18:19:30 +0900

    feat: saved page

--- jacareview/components/SavedRestaurants.tsx
+++ jacareview/components/SavedRestaurants.tsx
@@ -4,7 +4,7 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { initFirebase } from "@/firebase/firebaseapp";
 import { useRouter } from "next/navigation";
-import jacaPhone from '../public/jaca-phone.png'
+import Link from "next/link";
 
 
 export default function SavedRestaurants(){
@@ -103,16 +103,19 @@ export default function SavedRestaurants(){
         )}
         {fetchedData && (
           savedData.map((element: any, index: number) => (
-            <div className="flex flex-col items-center w-full shadow-xl w-11/12 mx-6 my-2 rounded bg-gradient-to-r from-green-200 from-10% via-green-100 via-30% to-green-100 to-90% text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl px-2 py-4" key={`a${index}`}>
+            <div className="flex flex-col items-center shadow-xl w-11/12 mx-6 my-2 rounded bg-gradient-to-r from-green-200 from-10% via-green-100 via-30% to-green-100 to-90% text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl px-2 py-4" key={`a${index}`}>
               <div className="flex flex-col items-center w-full font-semibold">{element.name}</div>
-              <button
-                onClick={getRestaurantID}
-                a-key={element.restaurant_id_id}
-                b-key={element.id}
-                className="w-full rounded bg-indigo-300 text-white px-4 py-2 mt-2"
-              >
-                Unsave
-              </button>
+                <Link key={`d${index}`} href={`/reviewpage/?restaurant=${element.restaurant_id_id}`} className="w-full mt-2 bg-indigo-100 text-indigo-500 p-2 rounded shadow-lg shadow-xl flex justify-center items-center">
+                    Review
+                </Link>
+                <button
+                    onClick={getRestaurantID}
+                    a-key={element.restaurant_id_id}
+                    b-key={element.id}
+                    className="w-full rounded bg-indigo-300 text-white px-4 py-2 mt-2"
+                >
+                    Unsave
+                </button>
             </div>
           ))
         )}
