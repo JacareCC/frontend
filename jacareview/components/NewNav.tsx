906a04384036145ae99a23ea996ac2fc3b58de39
GONZALEZ-RODRIGUES <gonzalez.rodrigues@gmail.com>
2023-12-07 15:19:31 +0900

    feat logout with transition

--- jacareview/components/NewNav.tsx
+++ jacareview/components/NewNav.tsx
@@ -12,89 +12,120 @@ import { initFirebase } from "@/firebase/firebaseapp";
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
 import {  signOut } from 'firebase/auth';
+import LogoutConfirm from "./header_components/LogoutConfirm";
+import LoadingAnimation from "./loading/Loading";
 
 
 function NewNav() {
-    
+    const [isLogoutModalOpen, setLogoutModalOpen] = useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [toggleLoading, setToggleLoading] = useState(false);
+
     initFirebase();
     const auth = getAuth(); 
     const [user, loading] = useAuthState(auth);
     const router = useRouter();
 
-    const handleLogout = async () => {
+    const handleLogout = () => {
+        setToggleMenu(false);
+        setLogoutModalOpen(true);
+    };
+    
+    const handleLogoutCancel = () => {
+        setLogoutModalOpen(false);
+    };
+    
+    const handleLogoutConfirm = async () => {
         const auth = getAuth();
+        setToggleLoading(true)
+        setTimeout(async () => {
         await signOut(auth);
-        router.push('/');
+
+            router.push('/');
+        }, 2000)
     };
 
-  const [toggleMenu, setToggleMenu] = useState(false);
-
   return (
-    <div className="">
-      <nav>
-        <div className="mx-auto">
-          <div className="flex mx-auto justify-between">
-          <div className="flex items-center md:justify-around w-full mx-auto my-12 md:ml-8 md:mr-16 lg:ml-4 lg:mr-8 xl:ml-8 xl:mr-4">
+    <>
+    
+    {toggleLoading ? (
 
-              <div>
-                <a
-                  href="/"
-                  className="flex gap-1 font-bold text-gray-700 items-center "
-                >
-                  <img className="h-16 w-16 text-primary mx-6 " src="/logo-home-.png"  />
-                  <span className="font-yaro text-jgreen text-2xl" >jacareview</span>
-                </a>
-              </div>
-              {/* primary */}
-              <div className="hidden lg:p-6  lg:flex lg:justify-between lg:gap-12 text-jgreen">
-                {user && (
-                <><a href="/search">Search</a><a href="/user">Profile</a><a href="/restaurants/saved">Saved Restaurants</a><a href="/user/business">My Business</a><button onClick={handleLogout} className="text-jgreen">
-                                      Logout
-                                  </button></>
-                    )}
-              </div>
-            </div>
-            {/* secondary */}
-            <div className="flex gap-6 mr-1 md:mr-5">
-                
-              <div className="hidden xs:flex items-center gap-10">
-                <div className="hidden lg:flex items-center gap-2">
-                  <MoonIcon className="h-6 w-6" />
-                  <SunIcon className="h-6 w-6" />
+        <LoadingAnimation />
+    ) : (
+        <div className="">
+        <nav>
+            <div className="mx-auto">
+                <div className="flex mx-auto justify-between">
+                    <div className="flex items-center md:justify-around w-full mx-auto my-12 md:ml-8 md:mr-16 lg:ml-4 lg:mr-8 xl:ml-8 xl:mr-4">
+
+                        <div>
+                            <a
+                                href="/"
+                                className="flex gap-1 font-bold text-gray-700 items-center "
+                            >
+                                <img className="h-16 w-16 text-primary mx-6 " src="/logo-home-.png" />
+                                <span className="font-yaro text-jgreen text-2xl">jacareview</span>
+                            </a>
+                        </div>
+                        {/* primary */}
+                        <div className="hidden lg:p-6  lg:flex lg:justify-between lg:gap-12 text-jgreen">
+                            {user && (
+                                <><a href="/search">Search</a><a href="/user">Profile</a><a href="/restaurants/saved">Saved Restaurants</a><a href="/user/business">My Business</a><button onClick={handleLogout} className="text-jgreen">
+                                    Logout
+                                </button></>
+                            )}
+                        </div>
+                    </div>
+                    {/* secondary */}
+                    <div className="flex gap-6 mr-1 md:mr-5">
+
+                        <div className="hidden xs:flex items-center gap-10">
+                            <div className="hidden lg:flex items-center gap-2">
+                                <MoonIcon className="h-6 w-6" />
+                                <SunIcon className="h-6 w-6" />
+                            </div>
+                            <div>
+                                <button className="rounded-full border-solid border-2 border-gray-300 py-2 px-4 hover:bg-gray-700 hover:text-gray-100">
+                                    Free Trial
+                                </button>
+                            </div>
+                        </div>
+                        {/* Mobile navigation toggle */}
+                        <div className="lg:hidden flex items-center m-4 text-jgreen">
+                            <button onClick={() => setToggleMenu(!toggleMenu)}>
+                                <Bars3Icon className="h-6" />
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                  <button className="rounded-full border-solid border-2 border-gray-300 py-2 px-4 hover:bg-gray-700 hover:text-gray-100">
-                    Free Trial
-                  </button>
+            </div>
+            {/* mobile navigation */}
+            <div
+                className={`fixed z-20 w-full  bg-white overflow-hidden flex flex-col lg:hidden gap-12  origin-top duration-700 ${!toggleMenu ? "h-0" : "h-full"}`}
+            >
+                <div className="px-8">
+                    <div className="flex flex-col justify-end gap-8 font-bold tracking-wider text-jgreen">
+                        <a href="/search">Search</a>
+                        <a href="/user">Profile</a>
+                        <a href="/restaurants/saved">Saved Restaurants</a>
+                        <a href="/user/business">My business</a>
+                        <a href="#" onClick={handleLogout}>Logout</a>
+                    </div>
                 </div>
-              </div>
-              {/* Mobile navigation toggle */}
-              <div className="lg:hidden flex items-center m-4 text-jgreen">
-                <button onClick={() => setToggleMenu(!toggleMenu)}>
-                  <Bars3Icon className="h-6" />
-                </button>
-              </div>
             </div>
-          </div>
+        </nav>
+        <div className="">
+          <LogoutConfirm
+                  open={isLogoutModalOpen}
+                  onClose={handleLogoutCancel}
+                  onConfirm={handleLogoutConfirm} 
+              />
         </div>
-        {/* mobile navigation */}
-        <div
-          className={`fixed z-40 w-full  bg-white overflow-hidden flex flex-col lg:hidden gap-12  origin-top duration-700 ${
-            !toggleMenu ? "h-0" : "h-full"
-          }`}
-        >
-          <div className="px-8">
-            <div className="flex flex-col gap-8 font-bold tracking-wider text-jgreen">
-              <a href="/search">Search</a>
-              <a href="/user">Profile</a>
-              <a href="/restaurants/saved">Saved Restaurants</a>
-              <a href="/user/business">My business</a>
-              <a href="#" onClick={handleLogout}>Logout</a>
-            </div>
-          </div>
-        </div>
-      </nav>
+
     </div>
+    )}
+    
+        </>
   );
 }
 
