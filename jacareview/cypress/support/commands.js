df3598e14559471693a2aff18969676e03c862e1
Kai Eugene <kai.yougene@gmail.com>
2024-01-14 19:37:37 +0900

    fix: api key into env variable

--- jacareview/cypress/support/commands.js
+++ jacareview/cypress/support/commands.js
@@ -5,15 +5,14 @@ import "firebase/compat/firestore";
 import { attachCustomCommands } from "cypress-firebase";
 
 const fbConfig = {
-  apiKey: "AIzaSyAmzWPMeTfPbz6BYOc8QoUcVnRYc_D80VA",
+  apiKey: process.env.API_KEY,
   authDomain: "jacareview.firebaseapp.com",
   projectId: "jacareview",
   storageBucket: "jacareview.appspot.com",
   messagingSenderId: "442408349281",
   appId: "1:442408349281:web:58526bc92231acc6c00420",
-  measurementId: "G-77KV183TNS"
+  measurementId: "G-77KV183TNS",
 };
-
 
 firebase.initializeApp(fbConfig);
 
