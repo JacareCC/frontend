8dadaae6f74542ae72c705785dab934509c3ecb1
Kai Eugene <kai.yougene@gmail.com>
2024-01-14 19:43:12 +0900

    Revert "fix: api key into env variable"
    This reverts commit df3598e14559471693a2aff18969676e03c862e1.

--- jacareview/cypress/support/commands.js
+++ jacareview/cypress/support/commands.js
@@ -5,14 +5,15 @@ import "firebase/compat/firestore";
 import { attachCustomCommands } from "cypress-firebase";
 
 const fbConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: "AIzaSyAmzWPMeTfPbz6BYOc8QoUcVnRYc_D80VA",
   authDomain: "jacareview.firebaseapp.com",
   projectId: "jacareview",
   storageBucket: "jacareview.appspot.com",
   messagingSenderId: "442408349281",
   appId: "1:442408349281:web:58526bc92231acc6c00420",
-  measurementId: "G-77KV183TNS",
+  measurementId: "G-77KV183TNS"
 };
+
 
 firebase.initializeApp(fbConfig);
 
--- jacareview/firebase/firebaseapp.ts
+++ jacareview/firebase/firebaseapp.ts
@@ -6,18 +6,19 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: "AIzaSyAmzWPMeTfPbz6BYOc8QoUcVnRYc_D80VA",
   authDomain: "jacareview.firebaseapp.com",
   projectId: "jacareview",
   storageBucket: "jacareview.appspot.com",
   messagingSenderId: "442408349281",
   appId: "1:442408349281:web:58526bc92231acc6c00420",
-  measurementId: "G-77KV183TNS",
+  measurementId: "G-77KV183TNS"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
-export function initFirebase() {
+
+export function initFirebase (){
   return app;
 }
