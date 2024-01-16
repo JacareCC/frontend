e7286b7ad2290610396cf19fc56dbaaa49556a22
Kai Eugene <kai.yougene@gmail.com>
2024-01-16 22:33:41 +0900

    fix: type interface

--- jacareview/app/search/page.tsx
+++ jacareview/app/search/page.tsx
@@ -7,7 +7,7 @@ import { initFirebase } from "@/firebase/firebaseapp";
 import { useRouter } from "next/navigation";
 import ColorChangingButton from "@/components/buttons/ColorChangingButton";
 import "../globals.css";
-import FunSearchButton from "@/components/loading/funSearchButton/FunSearchButton"
+import FunSearchButton from "@/components/loading/funSearchButton/FunSearchButton";
 import LoadingAnimation from "@/components/loading/Loading";
 import VerifyUser from "../globalfunctions/TokenVerification";
 import Slideshow from "@/components/SlideShow";
@@ -16,12 +16,17 @@ import NewNav from "@/components/navbarComponents/NewNav";
 import LocationPopup from "@/components/popUpComponents/LocationOnPopUp";
 import NoResultsPopup from "@/components/popUpComponents/NoResultsPopUp";
 import ModeOfTransportButton from "@/components/buttons/ModeOfTransportButton";
-import {Coordinates} from "../../typeInterfaces/globals";
+import { Coordinates } from "../../typeInterfaces/globals";
 import { GeolibInputCoordinates } from "geolib/es/types";
 
-
-
-
+interface searchDataObject {
+  cuisineOptions: string[] | null;
+  price: number | null;
+  openNow: boolean | null;
+  amountOfOptions: number | null;
+  distanceToTravel: number | null;
+  location: GeolibInputCoordinates | null;
+}
 
 export default function SearchPage() {
   const [location, setLocation] = useState<GeolibInputCoordinates | null>(null);
@@ -39,8 +44,7 @@ export default function SearchPage() {
   const [includeOthers, setIncludeOthers] = useState<boolean | null>(null);
   const [searchClicked, setSearchClicked] = useState<boolean>(false);
   const [turnOnLocation, setTurnOnLocation] = useState<boolean>(false);
-  const [modeOfTransport, setModeOfTransport] = useState<string> ("Walking");
-
+  const [modeOfTransport, setModeOfTransport] = useState<string>("Walking");
 
   initFirebase();
   const auth = getAuth();
@@ -56,31 +60,21 @@ export default function SearchPage() {
   });
 
   useEffect(() => {
-    if(modeOfTransport === "Walking"){
+    if (modeOfTransport === "Walking") {
       setDistanceToTravel(500);
     }
-    if(modeOfTransport === "Car"){
+    if (modeOfTransport === "Car") {
       setDistanceToTravel(12500);
     }
-  }, [modeOfTransport])
+  }, [modeOfTransport]);
 
-  useEffect(()=>{
-    if(statusCode && statusCode !== 200){
-      router.push("/")
-    }
-    else if(statusCode=== 200){
+  useEffect(() => {
+    if (statusCode && statusCode !== 200) {
+      router.push("/");
+    } else if (statusCode === 200) {
       setStatusCodeOk(true);
     }
-  },[statusCode])
-
-  interface searchDataObject {
-    cuisineOptions: string[] | null;
-    price: number | null;
-    openNow: boolean | null;
-    amountOfOptions: number | null;
-    distanceToTravel: number | null;
-    location: GeolibInputCoordinates | null;
-  }
+  }, [statusCode]);
 
   const searchObject: searchDataObject = {
     cuisineOptions: cuisineType,
@@ -89,7 +83,6 @@ export default function SearchPage() {
     amountOfOptions: amountOfOptions,
     distanceToTravel: distanceToTravel,
     location: location,
-
   };
 
   useEffect(() => {
@@ -101,19 +94,17 @@ export default function SearchPage() {
   }, [price, cuisineType]);
 
   useEffect(() => {
-      searchObject.cuisineOptions = ["restaurant"];
-      searchObject.price = 2;
-      searchObject.openNow = true;
-      searchObject.amountOfOptions = 3;
-      searchObject.distanceToTravel = 500;
+    searchObject.cuisineOptions = ["restaurant"];
+    searchObject.price = 2;
+    searchObject.openNow = true;
+    searchObject.amountOfOptions = 3;
+    searchObject.distanceToTravel = 500;
   }, [resetCount]);
-  
+
   useEffect(() => {
-   
     requestGeolocation();
   }, []);
-  
- 
+
   const requestGeolocation = async () => {
     if ("geolocation" in navigator) {
       try {
@@ -125,14 +116,12 @@ export default function SearchPage() {
         const { latitude, longitude }: Coordinates = coords;
         setLocation({ latitude, longitude });
       } catch (error) {
-        if(error instanceof Error)
-        console.error("Error getting geolocation:", error.message);
+        if (error instanceof Error)
+          console.error("Error getting geolocation:", error.message);
         // Handle errors
       }
     }
   };
-  
-
 
   useEffect(() => {
     if (results) {
@@ -140,160 +129,199 @@ export default function SearchPage() {
     }
   }, [results]);
 
-
-
-async function fetchRestaurants() {
-    
+  async function fetchRestaurants() {
     const results = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}search/`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `${user?.uid}`
-        },
-        body: JSON.stringify(searchObject),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${user?.uid}`,
+      },
+      body: JSON.stringify(searchObject),
+    })
+      .then((response) => {
+        setStatusCode(response.status);
+        return response.json();
       })
-        .then((response) => {
-          setStatusCode(response.status);
-          return response.json();
-        })
-        .then((data) => { 
-          setResults(data);
-        });
-}
+      .then((data) => {
+        setResults(data);
+      });
+  }
 
   async function handleSubmitWithLocation() {
-    if(location){
-    fetchRestaurants();
-    setSearchClicked((prev:boolean) => !prev);
+    if (location) {
+      fetchRestaurants();
+      setSearchClicked((prev: boolean) => !prev);
     }
-    if(!location){
-      setTurnOnLocation((prev:boolean) => !prev)
+    if (!location) {
+      setTurnOnLocation((prev: boolean) => !prev);
     }
-  
   }
 
   async function handleSubmitWithLocationOne() {
     searchObject.amountOfOptions = 1;
-    if(location){
+    if (location) {
       fetchRestaurants();
-      setSearchClicked((prev:boolean) => !prev);
-      }
-      if(!location){
-        setTurnOnLocation((prev:boolean) => !prev)
-      }
-    
+      setSearchClicked((prev: boolean) => !prev);
+    }
+    if (!location) {
+      setTurnOnLocation((prev: boolean) => !prev);
+    }
   }
 
-  
   return (
     <>
-    <div className="h-[calc(100dvh)">
-    {turnOnLocation && (<LocationPopup setTurnOnLocation={setTurnOnLocation}/>)}
-    {statusCodeOK && (<div className="">
-      <NewNav />
-    </div>)}
-    <div className="flex flex-col container mx-auto md: mt-0 shadow-2xl bg-test rounded">
-        {!resultsFetched && (
-          <div className="flex flex-col  gap-4 md:flex-row  flex-grow justify-around items-center rounded ">
-           
-            {!statusCodeOK ? (
-             <div className="fixed top-0 right-0 left-0 h-screen w-screen flex items-center justify-center bg-white">
-               <LoadingAnimation />
-              </div>
-            ) : (
-              <>
-                {!resultsFetched && !searchClicked ? (
-              
-                  <>
-                    <div className="sm:mt-0 md:w-1/2  flex flex-col items-center justify-center">
-                      <img className="md:rounded-l" src="./gator-searching.png" alt="Gator Searching" />
-                    </div>
-                    <div className=" p-4 sm:p-0 md:flex flex-col items-center justify-center w-full md:w-1/2">
-                      {/* Section 1 */}
-                      <div className="flex flex-col items-center justify-center md:pt-4"></div>
-                      <h1 className="text-2xl font-bold text-jgreen mb-2border-b flex flex-col items-center justify-center">Search</h1>
-                      <div className="flex items-center justify-center ml-4 mr-4 mb-2 space-x-4 md:space-x-8">
-                        <FunSearchButton text="One JacaRestaurant" fetchData={handleSubmitWithLocationOne} />
-                        <FunSearchButton text="Three JacaRestaurants" fetchData={handleSubmitWithLocation} />
-                      </div>
-                      {/* Section 2 */}
-                      <div className="md:flex flex-col md: justify-center items-center">
-                      <div className="flex flex-col items-center justify-center">
-                        <h1 className="text-xl font-bold text-jgreen mb-2 border-b">Max Price</h1>
-                        <div className="flex flex-row">
-                          <PriceButton setPrice={setPrice} price={price} text={"$"}  />
-                          <PriceButton setPrice={setPrice} price={price} text={"$$"} />
-                          <PriceButton setPrice={setPrice} price={price} text={"$$$"} />
-                          <PriceButton setPrice={setPrice} price={price} text={"$$$$"} />
-                        </div>
-                      </div>
-                      <div className="flex flex-col items-center justify-center mb-8">
-                        {/* Section 3 */}
-                        <div className="flex flex-col items-center justify-center mb-0 m-4">
-                          <h1 className="text-xl font-bold text-jgreen mb-2 border-b">Dietary Restrictions</h1>
-                          <div className="flex flex-row">
-                            <ColorChangingButton text={"Vegan"}
-                              setCuisineType={setCuisineType}
-                              cuisineType={cuisineType}
-
-                              includeOthers={includeOthers}
-
-                              count={count}
-                              setCount={setCount}
-                              setIncludeOthers={setIncludeOthers}
-                              resetCount={resetCount} />
-                            <ColorChangingButton text={"Vegetarian"}
-                              setCuisineType={setCuisineType}
-                              cuisineType={cuisineType}
-                              includeOthers={includeOthers}
-                              count={count}
-                              setCount={setCount}
-                              setIncludeOthers={setIncludeOthers}
-                              resetCount={resetCount} />
-                          </div>
-                        </div>
-                        </div>
-
-                        <div className="flex flex-col items-center justify-center mb-8">
-                        {/* Section 4 */}
-                        <div className="flex flex-col items-center justify-center mb-0 m-4">
-                          <h1 className="text-xl font-bold text-jgreen mb-2 border-b">Dietary Restrictions</h1>
-                          <div className="flex flex-row">
-                            <ModeOfTransportButton setModeOfTransport={setModeOfTransport} modeOfTransport={modeOfTransport} text="Walking"/>
-                            <ModeOfTransportButton setModeOfTransport={setModeOfTransport} modeOfTransport={modeOfTransport} text="Car"/>
-                          </div>
-                        </div>
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                ) : (
-
-                  <div className="z-10 fixed top-0 right-0 bottom-0 left-0 bg-white flex justify-center items-center">
-                    {!resultsFetched && (
-                      <div className=" relative w-80 h-80 md:w-96 md:h-96 lg:w-120 lg:h-120 xl:w-160 xl:h-160 overflow-hidden">
+      <div className="h-[calc(100dvh)">
+        {turnOnLocation && (
+          <LocationPopup setTurnOnLocation={setTurnOnLocation} />
+        )}
+        {statusCodeOK && (
+          <div className="">
+            <NewNav />
+          </div>
+        )}
+        <div className="flex flex-col container mx-auto md: mt-0 shadow-2xl bg-test rounded">
+          {!resultsFetched && (
+            <div className="flex flex-col  gap-4 md:flex-row  flex-grow justify-around items-center rounded ">
+              {!statusCodeOK ? (
+                <div className="fixed top-0 right-0 left-0 h-screen w-screen flex items-center justify-center bg-white">
+                  <LoadingAnimation />
+                </div>
+              ) : (
+                <>
+                  {!resultsFetched && !searchClicked ? (
+                    <>
+                      <div className="sm:mt-0 md:w-1/2  flex flex-col items-center justify-center">
                         <img
-                          src="https://media.giphy.com/media/VQUo8CBVIRliuz1TNI/giphy.gif"
-                          alt="Alligator eating a star"
-                          className="w-full h-full object-cover rounded-full border-4 border-emerald-500" />
+                          className="md:rounded-l"
+                          src="./gator-searching.png"
+                          alt="Gator Searching"
+                        />
                       </div>
-                    )}
-                  </div>
-                )}
-              </>
-            )}
-          </div>
-        )}
-        {resultsFetched && results.result.length === 0 ? <NoResultsPopup/> :(
-          
-          <div className="flex-grow">
-           
-            <Slideshow slides={results} location={location} user={user} />
-          </div>
-        )}
+                      <div className=" p-4 sm:p-0 md:flex flex-col items-center justify-center w-full md:w-1/2">
+                        {/* Section 1 */}
+                        <div className="flex flex-col items-center justify-center md:pt-4"></div>
+                        <h1 className="text-2xl font-bold text-jgreen mb-2border-b flex flex-col items-center justify-center">
+                          Search
+                        </h1>
+                        <div className="flex items-center justify-center ml-4 mr-4 mb-2 space-x-4 md:space-x-8">
+                          <FunSearchButton
+                            text="One JacaRestaurant"
+                            fetchData={handleSubmitWithLocationOne}
+                          />
+                          <FunSearchButton
+                            text="Three JacaRestaurants"
+                            fetchData={handleSubmitWithLocation}
+                          />
+                        </div>
+                        {/* Section 2 */}
+                        <div className="md:flex flex-col md: justify-center items-center">
+                          <div className="flex flex-col items-center justify-center">
+                            <h1 className="text-xl font-bold text-jgreen mb-2 border-b">
+                              Max Price
+                            </h1>
+                            <div className="flex flex-row">
+                              <PriceButton
+                                setPrice={setPrice}
+                                price={price}
+                                text={"$"}
+                              />
+                              <PriceButton
+                                setPrice={setPrice}
+                                price={price}
+                                text={"$$"}
+                              />
+                              <PriceButton
+                                setPrice={setPrice}
+                                price={price}
+                                text={"$$$"}
+                              />
+                              <PriceButton
+                                setPrice={setPrice}
+                                price={price}
+                                text={"$$$$"}
+                              />
+                            </div>
+                          </div>
+                          <div className="flex flex-col items-center justify-center mb-8">
+                            {/* Section 3 */}
+                            <div className="flex flex-col items-center justify-center mb-0 m-4">
+                              <h1 className="text-xl font-bold text-jgreen mb-2 border-b">
+                                Dietary Restrictions
+                              </h1>
+                              <div className="flex flex-row">
+                                <ColorChangingButton
+                                  text={"Vegan"}
+                                  setCuisineType={setCuisineType}
+                                  cuisineType={cuisineType}
+                                  includeOthers={includeOthers}
+                                  count={count}
+                                  setCount={setCount}
+                                  setIncludeOthers={setIncludeOthers}
+                                  resetCount={resetCount}
+                                />
+                                <ColorChangingButton
+                                  text={"Vegetarian"}
+                                  setCuisineType={setCuisineType}
+                                  cuisineType={cuisineType}
+                                  includeOthers={includeOthers}
+                                  count={count}
+                                  setCount={setCount}
+                                  setIncludeOthers={setIncludeOthers}
+                                  resetCount={resetCount}
+                                />
+                              </div>
+                            </div>
+                          </div>
+
+                          <div className="flex flex-col items-center justify-center mb-8">
+                            {/* Section 4 */}
+                            <div className="flex flex-col items-center justify-center mb-0 m-4">
+                              <h1 className="text-xl font-bold text-jgreen mb-2 border-b">
+                                Dietary Restrictions
+                              </h1>
+                              <div className="flex flex-row">
+                                <ModeOfTransportButton
+                                  setModeOfTransport={setModeOfTransport}
+                                  modeOfTransport={modeOfTransport}
+                                  text="Walking"
+                                />
+                                <ModeOfTransportButton
+                                  setModeOfTransport={setModeOfTransport}
+                                  modeOfTransport={modeOfTransport}
+                                  text="Car"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  ) : (
+                    <div className="z-10 fixed top-0 right-0 bottom-0 left-0 bg-white flex justify-center items-center">
+                      {!resultsFetched && (
+                        <div className=" relative w-80 h-80 md:w-96 md:h-96 lg:w-120 lg:h-120 xl:w-160 xl:h-160 overflow-hidden">
+                          <img
+                            src="https://media.giphy.com/media/VQUo8CBVIRliuz1TNI/giphy.gif"
+                            alt="Alligator eating a star"
+                            className="w-full h-full object-cover rounded-full border-4 border-emerald-500"
+                          />
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
+          )}
+          {resultsFetched && results.result.length === 0 ? (
+            <NoResultsPopup />
+          ) : (
+            <div className="flex-grow">
+              <Slideshow slides={results} location={location} user={user} />
+            </div>
+          )}
+        </div>
       </div>
-      </div>
-      </>
-  
-);
-      }  
+    </>
+  );
+}
