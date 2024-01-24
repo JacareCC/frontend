cc255b6fe1d48d83d7f1f2b4468642f5518b92ed
William Brammer <williamedwardbrammer@gmail.com>
2024-01-24 20:09:31 +0900

    fix api key to be hidden

--- jacareview/firebase/firebaseapp.ts
+++ jacareview/firebase/firebaseapp.ts
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAmzWPMeTfPbz6BYOc8QoUcVnRYc_D80VA",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE,
   authDomain: "jacareview.firebaseapp.com",
   projectId: "jacareview",
   storageBucket: "jacareview.appspot.com",
