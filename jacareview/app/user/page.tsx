2b0ca20c488d34c71142e7780ba5c72fab8153f0
Kai Eugene <kai.yougene@gmail.com>
2024-01-24 15:21:55 +0900

    fix: client side auth, pass history data as prop
    Co-authored-by: NabbeunNabi <williamedwardbrammer@gmail.com>

--- jacareview/app/user/page.tsx
+++ jacareview/app/user/page.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useAuthState } from "react-firebase-hooks/auth"
-import { initFirebase } from "@/firebase/firebaseapp"
+import { useAuthState } from "react-firebase-hooks/auth";
+import { initFirebase } from "@/firebase/firebaseapp";
 import { useRouter } from "next/navigation";
-import '../globals.css'
+import "../globals.css";
 import { useEffect, useState } from "react";
 import InfoUser from "@/components/userPage/InfoUser";
 import RestViewed from "@/components/userPage/RestViewed";
@@ -13,110 +13,110 @@ import ClaimButton from "@/components/userPage/ClaimButton";
 import NewNav from "@/components/navbarComponents/NewNav";
 import LoadingAnimation from "@/components/loading/Loading";
 
+export default function UserPage() {
+  const [userDataFetched, setUserDataFetched] = useState<boolean>(false);
 
-export default function UserPage(){
-    const[statusCode, setStatusCode] = useState<number|null>(null);
+  initFirebase();
+  const auth = getAuth();
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
 
-    initFirebase();
-    const auth = getAuth(); 
-    const [user, loading] = useAuthState(auth);
-    const router = useRouter();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      return;
+    } else {
+      router.push("/");
+    }
+  });
 
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-          return;
-        } else {
-          router.push("/");
-        }
-      });
-      
-        const[userPhoto, setUserPhoto] = useState<string | undefined>(undefined);
-        const[userName, setUserName] = useState<string | undefined>(undefined);
-        const[email, setEmail] = useState<string | undefined>(undefined);
-        const[birthday, setBirthday] = useState<string | undefined>(undefined);
-        const[uid, setUid] = useState<string|null |undefined> (null);
-        const[points, setPoints] = useState<string |null|undefined>(null);
+  const [userPhoto, setUserPhoto] = useState<string | undefined>(undefined);
+  const [userName, setUserName] = useState<string | undefined>(undefined);
+  const [email, setEmail] = useState<string | undefined>(undefined);
+  const [birthday, setBirthday] = useState<string | undefined>(undefined);
+  const [uid, setUid] = useState<string | null | undefined>(null);
+  const [points, setPoints] = useState<string | null | undefined>(null);
+  const [triggerRefresh, setTriggerRefresh] = useState<number | null>(null);
+  const [historyData, setHistoryData] = useState<any>(null);
 
-        useEffect(()=>{
-          if(statusCode && statusCode !== 200){
-            router.push("/")
-          }
+  useEffect(() => {
+    if (user) {
+      setUid(user?.uid);
+    }
+  }, [user]);
 
-        },[statusCode])
+  useEffect(() => {
+    if (uid && !userDataFetched) {
+      getUserData();
+    }
+  }, [uid]);
 
-        useEffect(() => {
-            if(user){
-            VerifyUser(user.uid, setStatusCode);
-            setUid(user?.uid)
-            }
-        }, [user]);
-
-        useEffect(()=>{
-          if(statusCode && statusCode !== 200){
-            router.push("/")
-          }
-        },[statusCode])
-    
-        useEffect(()=>{
-            if(uid){
-            getUserData();
-            }
-        }, [uid])
-
-        async function getUserData(){
-            const results = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}user/profile/`, {
-                method: 'GET',
-                headers: {
-                  "Content-Type": "application/json" , 
-                  "Authorization" : `${uid}`
-                }
-              })
-                  .then(response => {return response.json()})
-                  .then(data => {
-                    setEmail(data.success.user.email);
-                    setBirthday(data.success.user.birthday);
-                    if(data.success.user.username !== null) setUserName(data.success.user.username);
-                    setPoints(data.success.points);
-                })
-            }
-
-        useEffect (() => {
-          if(user) {
-            if(user.photoURL)
-            setUserPhoto(user?.photoURL)
-            if(user.displayName)
-            setUserName(user?.displayName)
-          }
-        }, [user]);
-
-    return(
-      <>
-      <>{!statusCode ? (
-        <div className="h-screen w-screen flex justify-center items-center">
-        <LoadingAnimation/>
-        </div>
-      ): null
-
-      }</>
-
-      { statusCode && statusCode === 200 && (   
-        <div>
-            <div className="">
-                <NewNav />
-            </div>
-            <div className="flex flex-col md:flex-row items-center container mx-auto md:shadow-2xl rounded">
-                <div className="w-full mb-4">
-                    <InfoUser email={email} birthday={birthday} name={userName} user_uid={uid} points={points}/>   
-                </div>
-                <div className="w-full">
-                    <RestViewed />
-                </div>
-            </div>
-                <div className=" flex flex-col md:flex-row items-center container mx-auto rounded pb-4">
-                    <ClaimButton user_uid={uid} />
-                </div>
-        </div>)
-}
-        </>
+  async function getUserData() {
+    const results = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}user/profile/`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${uid}`,
+        },
+      }
     )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setEmail(data.success.user.email);
+        setBirthday(data.success.user.birthday);
+        if (data.success.user.username !== null)
+          setUserName(data.success.user.username);
+        setPoints(data.success.points);
+        setHistoryData(data.success.history);
+        setTriggerRefresh(data.status);
+        setUserDataFetched((prev: boolean) => !prev);
+      });
+  }
+
+  useEffect(() => {
+    if (user) {
+      if (user.photoURL) setUserPhoto(user?.photoURL);
+      if (user.displayName) setUserName(user?.displayName);
+    }
+  }, [user]);
+
+  return (
+    <>
+      <>
+        {loading && !user ? (
+          <div className="h-screen w-screen flex justify-center items-center">
+            <LoadingAnimation />
+          </div>
+        ) : null}
+      </>
+
+      {!loading && user && (
+        <div>
+          <div className="">
+            <NewNav />
+          </div>
+          <div className="flex flex-col md:flex-row items-center container mx-auto md:shadow-2xl rounded">
+            <div className="w-full mb-4">
+              <InfoUser
+                email={email}
+                birthday={birthday}
+                name={userName}
+                user_uid={uid}
+                points={points}
+              />
+            </div>
+            <div className="w-full">
+              <RestViewed historyData={historyData} />
+            </div>
+          </div>
+          <div className=" flex flex-col md:flex-row items-center container mx-auto rounded pb-4">
+            <ClaimButton user_uid={uid} />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
